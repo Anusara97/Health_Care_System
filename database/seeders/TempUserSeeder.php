976521db630916d61a782cb6969706b9cae0b22d
Anusara97 <anusara777rcm@gmail.com>
2025-01-14 13:18:58 +0530

    Seeder files created.

--- database/seeders/TempUserSeeder.php
+++ database/seeders/TempUserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Hash;
 
 class TempUserSeeder extends Seeder
 {
@@ -13,5 +16,64 @@ class TempUserSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('temp_users')->insert([
+            'name'=>'Kalum Perera',
+            'age'=>34,
+            'telNo'=>'0712485617',
+            'email'=>'kalum@gmail.com',
+            'gender'=>'Male',
+            'nic'=>'911057313V',
+            'slmcNo'=>10756,
+            'role'=>'Doctor',
+            'password'=>Hash::make('kalum123')
+        ]);
+
+        DB::table('temp_users')->insert([
+            'name'=>'Asela Gamage',
+            'age'=>30,
+            'telNo'=>'0779205356',
+            'email'=>'asela@gmail.com',
+            'gender'=>'Male',
+            'nic'=>'952697917V',
+            'slmcNo'=>11296,
+            'role'=>'Senior Pharmacist',
+            'password'=>Hash::make('asela123')
+        ]);
+
+        DB::table('temp_users')->insert([
+            'name'=>'Samanthi Arosha',
+            'age'=>40,
+            'telNo'=>'0776276536',
+            'email'=>'samnthi@gmail.com',
+            'gender'=>'female',
+            'nic'=>'852657598V',
+            'slmcNo'=>12526,
+            'role'=>'Junior Pharmacist',
+            'password'=>Hash::make('samanthi123')
+        ]);
+
+        DB::table('temp_users')->insert([
+            'name'=>'Sirisana Gamlath',
+            'age'=>45,
+            'telNo'=>'0416276536',
+            'email'=>'sirisena@gmail.com',
+            'gender'=>'Male',
+            'nic'=>'802622750V',
+            'slmcNo'=>NULL,
+            'role'=>'Patient',
+            'password'=>Hash::make('sirisena123')
+        ]);
+
+        DB::table('temp_users')->insert([
+            'name'=>'Thilini Kaushalya',
+            'age'=>20,
+            'telNo'=>'0746204936',
+            'email'=>'thilini@gmail.com',
+            'gender'=>'female',
+            'nic'=>'20052657598V',
+            'slmcNo'=>NULL,
+            'role'=>'Patient',
+            'password'=>Hash::make('thilini123')
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +16,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('users')->insert([
+            'name'=>'Anusara Punchihewa',
+            'age'=>28,
+            'telNo'=>'0712245217',
+            'email'=>'anusara@gmail.com',
+            'gender'=>'Male',
+            'nic'=>'971446313V',
+            'slmcNo'=>NULL,
+            'role'=>'Admin',
+            'password'=>Hash::make('anusara123')
+        ]);
     }
 }
